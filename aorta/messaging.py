4faa1f1565462a3618ba9bfa92a92c9e87113031
Cochise Ruhulessin <cochiseruhulessin@gmail.com>
2018-02-19 11:09:08 +0100

    Add additional message properties

--- aorta/messaging.py
+++ aorta/messaging.py
@@ -21,15 +21,17 @@ class AortaMessage(Message):
         Message.__init__(self, *args, **kwargs)
         if not isinstance(self.properties, dict):
             self.properties = {
-                aorta.const.APROP_AORTA_ID: uuid.UUID(bytes=os.urandom(16)).hex
+                aorta.const.P_AORTA_ID: uuid.UUID(bytes=os.urandom(16)).hex,
+                aorta.const.P_ENCRYPTED: False,
+                aorta.const.P_SIGNED: False
             }
         assert self.message_class is not None,\
             "%s.message_class is None" % type(self).__name__
-        self.properties[aorta.const.APROP_MESSAGE_CLASS] = self.message_class
+        self.properties[aorta.const.P_MESSAGE_CLASS] = self.message_class
 
     def set_object_type(self, name):
         """Sets the object type in the application properties."""
-        self.properties[aorta.const.APROP_OBJECT_TYPE] = name
+        self.properties[aorta.const.P_OBJECT_TYPE] = name
 
 
 class EventMessage(AortaMessage):
