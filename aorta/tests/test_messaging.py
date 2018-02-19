0d3fda2a2804ad163e7c66c16e878bc9fb6bc0ad
Cochise Ruhulessin <cochiseruhulessin@gmail.com>
2018-02-19 17:50:20 +0100

    Prove that set_object_type() sets property

--- aorta/tests/test_messaging.py
+++ aorta/tests/test_messaging.py
@@ -1,6 +1,7 @@
 import unittest
 
 from aorta.const import P_MESSAGE_CLASS
+from aorta.const import P_OBJECT_TYPE
 from aorta.messaging import AortaMessage
 from aorta.messaging import EventMessage
 
@@ -21,3 +22,8 @@ class EventMessageTestCase(unittest.TestCase):
     def test_init_sets_amc_property(self):
         m = EventMessage()
         self.assertEqual(m.properties[P_MESSAGE_CLASS], 'event')
+
+    def test_set_object_type_sets_property(self):
+        m = EventMessage()
+        m.set_object_type('foo')
+        self.assertEqual(m.properties[P_OBJECT_TYPE], 'foo')
